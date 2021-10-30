8ec5a6d2d2e687f99a677b9e56730a85bae14f4a
Dan Egnor <egnor@ofb.net>
2021-10-30 00:45:16 -0700

    add stream dump to pivid_inspect_avformat

--- pivid_inspect_avformat.cpp
+++ pivid_inspect_avformat.cpp
@@ -18,38 +18,30 @@ extern "C" {
 #include <libavutil/rational.h>
 }
 
-DEFINE_bool(frames, false, "Print individual frames");
-
-void inspect_media(std::string const& filename) {
-    AVFormatContext* context = nullptr;
-    if (avformat_open_input(&context, filename.c_str(), nullptr, nullptr) < 0) {
-        fmt::print("*** {}: {}\n", filename, strerror(errno));
-        exit(1);
+void inspect_media(AVFormatContext* const avc) {
+    if (avformat_find_stream_info(avc, nullptr) < 0) {
+        fmt::print("*** Stream info ({}): {}\n", avc->url, strerror(errno));
     }
 
-    if (avformat_find_stream_info(context, nullptr) < 0) {
-        fmt::print("*** Stream info ({}): {}\n", filename, strerror(errno));
-    }
-
-    fmt::print("=== {} ===\n", context->url);
+    fmt::print("=== {} ===\n", avc->url);
     fmt::print("Container:");
-    if (context->duration)
-        fmt::print(" {:.1f}sec", context->duration * 1.0 / AV_TIME_BASE);
-    if (context->bit_rate)
-        fmt::print(" {}bps", context->bit_rate);
-    fmt::print(" ({})\n", context->iformat->long_name);
+    if (avc->duration)
+        fmt::print(" {:.1f}sec", avc->duration * 1.0 / AV_TIME_BASE);
+    if (avc->bit_rate)
+        fmt::print(" {}bps", avc->bit_rate);
+    fmt::print(" ({})\n", avc->iformat->long_name);
 
     AVDictionaryEntry* entry = nullptr;
     while ((entry = av_dict_get(
-        context->metadata, "", entry, AV_DICT_IGNORE_SUFFIX
+        avc->metadata, "", entry, AV_DICT_IGNORE_SUFFIX
     ))) {
         fmt::print("    {}: {}\n", entry->key, entry->value);
     }
     fmt::print("\n");
 
-    fmt::print("{} stream(s):\n", context->nb_streams);
-    for (uint32_t si = 0; si < context->nb_streams; ++si) {
-        auto const* stream = context->streams[si];
+    fmt::print("{} stream(s):\n", avc->nb_streams);
+    for (uint32_t si = 0; si < avc->nb_streams; ++si) {
+        auto const* stream = avc->streams[si];
         double const time_base = av_q2d(stream->time_base);
 
         fmt::print("    Str #{}", stream->id);
@@ -119,87 +111,123 @@ void inspect_media(std::string const& filename) {
         }
     }
     fmt::print("\n");
+}
 
+void list_frames(AVFormatContext* const avc) {
     AVPacket packet = {};
-    if (FLAGS_frames) {
-        fmt::print("--- Frames ---\n");
-        while (av_read_frame(context, &packet) >= 0) {
-            auto const* stream = context->streams[packet.stream_index];
-            fmt::print(
-                "S{} ({})",
-                packet.stream_index,
-                avcodec_get_name(stream->codecpar->codec_id)
-            );
-            double const time_base = av_q2d(stream->time_base);
-            fmt::print(" {:4d}kB", packet.size / 1024);
-            if (packet.duration != 0)
-                fmt::print(" {:.3f}s", packet.duration * time_base);
-            if (packet.pts != AV_NOPTS_VALUE)
-                fmt::print(" show@{:.3f}s", packet.pts * time_base);
-            if (packet.dts != AV_NOPTS_VALUE)
-                fmt::print(" deco@{:.3f}s", packet.dts * time_base);
+    fmt::print("--- Frames ---\n");
+    if (avformat_seek_file(avc, -1, 0, 0, 0, 0) < 0) {
+        fmt::print("*** Seek to start ({}): {}\n", avc->url, strerror(errno));
+        exit(1);
+    }
+    while (av_read_frame(avc, &packet) >= 0) {
+        auto const* stream = avc->streams[packet.stream_index];
+        fmt::print(
+            "S{} ({})",
+            packet.stream_index,
+            avcodec_get_name(stream->codecpar->codec_id)
+        );
+        double const time_base = av_q2d(stream->time_base);
+        fmt::print(" {:4d}kB", packet.size / 1024);
+        if (packet.duration != 0)
+            fmt::print(" {:.3f}s", packet.duration * time_base);
+        if (packet.pts != AV_NOPTS_VALUE)
+            fmt::print(" show@{:.3f}s", packet.pts * time_base);
+        if (packet.dts != AV_NOPTS_VALUE)
+            fmt::print(" deco@{:.3f}s", packet.dts * time_base);
 
-            for (uint32_t bit = 1; bit > 0; bit <<= 1) {
-                if ((packet.flags & bit)) {
-                    switch (bit) {
+        for (uint32_t bit = 1; bit > 0; bit <<= 1) {
+            if ((packet.flags & bit)) {
+                switch (bit) {
 #define F(X) case AV_PKT_FLAG_##X: fmt::print(" {}", #X); break
-                        F(KEY);
-                        F(CORRUPT);
-                        F(DISCARD);
-                        F(TRUSTED);
-                        F(DISPOSABLE);
+                    F(KEY);
+                    F(CORRUPT);
+                    F(DISCARD);
+                    F(TRUSTED);
+                    F(DISPOSABLE);
 #undef F
-                        default: fmt::print(" ?0x{:x}?", bit); break;
-                    }
+                    default: fmt::print(" ?0x{:x}?", bit); break;
                 }
             }
-
-            if (packet.side_data_elems) fmt::print(" /");
-            for (int si = 0; si < packet.side_data_elems; ++si) {
-                switch (packet.side_data[si].type) {
-#define S(X) case AV_PKT_DATA_##X: fmt::print(" {}", #X); break
-                    S(PALETTE);
-                    S(NEW_EXTRADATA);
-                    S(PARAM_CHANGE);
-                    S(H263_MB_INFO);
-                    S(REPLAYGAIN);
-                    S(DISPLAYMATRIX);
-                    S(STEREO3D);
-                    S(AUDIO_SERVICE_TYPE);
-                    S(QUALITY_STATS);
-                    S(FALLBACK_TRACK);
-                    S(CPB_PROPERTIES);
-                    S(SKIP_SAMPLES);
-                    S(JP_DUALMONO);
-                    S(STRINGS_METADATA);
-                    S(SUBTITLE_POSITION);
-                    S(MATROSKA_BLOCKADDITIONAL);
-                    S(WEBVTT_IDENTIFIER);
-                    S(WEBVTT_SETTINGS);
-                    S(METADATA_UPDATE);
-                    S(MPEGTS_STREAM_ID);
-                    S(MASTERING_DISPLAY_METADATA);
-                    S(SPHERICAL);
-                    S(CONTENT_LIGHT_LEVEL);
-                    S(A53_CC);
-                    S(ENCRYPTION_INIT_INFO);
-                    S(ENCRYPTION_INFO);
-                    S(AFD);
-#undef S
-                    default: fmt::print(" ?side%d?", packet.side_data[si].type);
-                }
-            }
-
-            fmt::print("\n");
-            av_packet_unref(&packet);
         }
+
+        if (packet.side_data_elems) fmt::print(" /");
+        for (int si = 0; si < packet.side_data_elems; ++si) {
+            switch (packet.side_data[si].type) {
+#define S(X) case AV_PKT_DATA_##X: fmt::print(" {}", #X); break
+                S(PALETTE);
+                S(NEW_EXTRADATA);
+                S(PARAM_CHANGE);
+                S(H263_MB_INFO);
+                S(REPLAYGAIN);
+                S(DISPLAYMATRIX);
+                S(STEREO3D);
+                S(AUDIO_SERVICE_TYPE);
+                S(QUALITY_STATS);
+                S(FALLBACK_TRACK);
+                S(CPB_PROPERTIES);
+                S(SKIP_SAMPLES);
+                S(JP_DUALMONO);
+                S(STRINGS_METADATA);
+                S(SUBTITLE_POSITION);
+                S(MATROSKA_BLOCKADDITIONAL);
+                S(WEBVTT_IDENTIFIER);
+                S(WEBVTT_SETTINGS);
+                S(METADATA_UPDATE);
+                S(MPEGTS_STREAM_ID);
+                S(MASTERING_DISPLAY_METADATA);
+                S(SPHERICAL);
+                S(CONTENT_LIGHT_LEVEL);
+                S(A53_CC);
+                S(ENCRYPTION_INIT_INFO);
+                S(ENCRYPTION_INFO);
+                S(AFD);
+#undef S
+                default: fmt::print(" ?side%d?", packet.side_data[si].type);
+            }
+        }
+
         fmt::print("\n");
+        av_packet_unref(&packet);
+    }
+    fmt::print("\n");
+}
+
+void dump_stream(
+    AVFormatContext* const avc, AVStream* const stream,
+    std::string const& prefix
+) {
+    auto const filename = fmt::format(
+        "{}.{}.{}", prefix, stream->index,
+        avcodec_get_name(stream->codecpar->codec_id)
+    );
+
+    fmt::print("Dumping stream #{} => {} ...\n", stream->index, filename);
+    FILE* const out = fopen(filename.c_str(), "wb");
+    if (out == nullptr) {
+        fmt::print("*** {}: {}\n", filename, strerror(errno));
+        exit(1);
     }
 
-    avformat_close_input(&context);
+    if (avformat_seek_file(avc, stream->index, 0, 0, 0, 0) < 0) {
+        fmt::print("*** Seek to start ({}): {}\n", avc->url, strerror(errno));
+        exit(1);
+    }
+
+    AVPacket packet = {};
+    while (av_read_frame(avc, &packet) >= 0) {
+        if (packet.stream_index != stream->index) continue;
+        fwrite(packet.data, packet.size, 1, out);
+    }
+
+    fclose(out);
 }
 
 DEFINE_string(media, "", "Media file or URL to inspect");
+
+DEFINE_bool(list_frames, false, "Print individual frames");
+
+DEFINE_string(dump_streams, "", "Dump raw streams to individual files");
 
 int main(int argc, char** argv) {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
@@ -207,6 +235,22 @@ int main(int argc, char** argv) {
         fmt::print("*** Usage: pivid_inspect_avformat --media=<mediafile>\n");
         exit(1);
     }
-    inspect_media(FLAGS_media);
+
+    AVFormatContext* avc = nullptr;
+    if (avformat_open_input(&avc, FLAGS_media.c_str(), nullptr, nullptr) < 0) {
+        fmt::print("*** {}: {}\n", FLAGS_media, strerror(errno));
+        exit(1);
+    }
+
+    inspect_media(avc);
+    if (FLAGS_list_frames) list_frames(avc);
+    if (!FLAGS_dump_streams.empty()) {
+        for (uint32_t si = 0; si < avc->nb_streams; ++si) {
+            dump_stream(avc, avc->streams[si], FLAGS_dump_streams);
+        }
+        fmt::print("\n");
+    }
+
+    avformat_close_input(&avc);
     return 0;
 }
