3207fefdd24f004e50c5cb1229834f29d9d015a9
Dan Egnor <egnor@ofb.net>
2021-12-19 20:10:34 -0800

    add a method

--- media_decoder.h
+++ media_decoder.h
@@ -13,6 +13,7 @@ class MediaFrame {
 class MediaDecoder {
  public:
   virtual ~MediaDecoder() {}
+  std::unique_ptr<MediaFrame> next_frame() = 0;
 };
 
 std::unique_ptr<MediaDecoder> new_media_decoder(const std::string& url);
