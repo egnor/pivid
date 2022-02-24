723378fab627c26d70aba8bfc756274c44485510
Dan Egnor <egnor@ofb.net>
2022-02-23 23:26:51 -0800

    start of script

--- geometry.h
+++ geometry.h
@@ -1,6 +1,10 @@
-// X/Y coordinate manipulation.
+// X/Y coordinate and path manipulation.
 
 #pragma once
+
+#include <optional>
+#include <utility>
+#include <vector>
 
 namespace pivid {
 
@@ -19,5 +23,32 @@ struct XY {
     template <typename U> XY operator*(U m) const { return {x * m, y * m}; }
     template <typename U> XY operator/(U d) const { return {x / d, y / d}; }
 };
+
+// Piecewise-cubic Bezier curve in x parameterized on t.
+struct CubicBezier {
+    // One (t, x) pair.
+    struct Point { double t, x; };
+
+    // A cubic segment defined by four Bezier control points nondecreasing in t.
+    struct Segment { ControlPoint begin, p1, p2, end; };
+
+    std::vector<Segment> segments;  // Nonoverlapping and increasing in t.
+    double repeat_every = 0.0;      // If nonzero, repeat the curve infinitely.
+};
+
+// These return f(t) from a Bezier segment or function, if t is in a segment.
+std::optional<double> value_at(CubicBezier::Segment const&, double t);
+std::optional<double> value_at(CubicBezier const&, double t);
+
+// Returns min & max f(t) across [t0, t1] intersected with a segment.
+std::optional<std::pair<double, double>> minmax_over(
+    CubicBezier::Segment const&, double t0, double t1
+);
+
+// Returns min & max f(t) for each range-noncontiguous intersection
+// of [t0, t1] with a Bezier function.
+std::vector<std::pair<double, double>> minmax_over(
+    CubicBezier const&, double t0, double t1
+);
 
 }  // namespace pivid
---
+++ script.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "geometry.h"
+
+namespace pivid {
+
+struct Script {
+    struct Layer {
+        std::string media_file;
+        CubicBezier frame_time;
+        XY<CubicBezier> from, from_size;
+        XY<CubicBezier> to, to_size;
+        CubicBezier alpha;
+    };
+
+    struct Display {
+        XY<int> mode_size;
+        std::vector<Layer> layers;
+    };
+
+    std::map<std::string, Display> displays;
+};
+
+}  // namespace pivid
