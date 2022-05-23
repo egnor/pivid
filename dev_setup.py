2e519dbe87cd65ab084a6c0bb84e1b6eb8964623
Dan Egnor <egnor@ofb.net>
2022-05-23 09:24:57 -0700

    run conan within direnv to ensure venv activation so it can run meson, etc

--- dev_setup.py
+++ dev_setup.py
@@ -47,56 +47,52 @@ if not all(
 
 print()
 print(f"=== C++ package manager (conan init) ===")
-conan_bin = venv_bin / "conan"
+run_conan = lambda *av: check_call(["direnv", "exec", source_dir, "conan", *av])
 conan_profile = build_dir / "conan-profile.txt"
 conan_install = build_dir / "conan-install"
-# os.environ["CONAN_V2_MODE"] = "1"  # doctest doesn't support
-os.environ["CONAN_USER_HOME"] = str(build_dir)
 
-check_call([conan_bin, "config", "init"])
-check_call([conan_bin, "config", "set", "general.revisions_enabled=1"])
+run_conan("config", "init")
+run_conan("config", "set", "general.revisions_enabled=1")
 if not conan_profile.is_file():
-    check_call([
-        conan_bin, "profile", "new", "--detect", "--force", conan_profile
-    ])
-    check_call([
-        conan_bin, "profile", "update", "settings.compiler.libcxx=libstdc++11",
+    run_conan("profile", "new", "--detect", "--force", conan_profile)
+    run_conan(
+        "profile", "update", "settings.compiler.libcxx=libstdc++11",
         conan_profile
-    ])
+    )
 
 for dir, ref in [
     ("ffmpeg_rpi_recipe", "ffmpeg/4.3+rpi@pivid/specific"),
 ]:
     print()
     print(f"=== {ref} recipe (conan export) ===")
-    check_call([conan_bin, "export", source_dir / dir, ref])
+    run_conan("export", source_dir / dir, ref)
 
 print()
 print(f"=== C++ dependencies (conan install) ===")
-check_call([
-    conan_bin, "install",
+run_conan(
+    "install",
     f"--profile={conan_profile}",
     # "--settings=build_type=Debug",  # Uncomment & re-run to build debug
     "--settings=ffmpeg:build_type=Release",  # ffmpeg ARM won't build Debug
     f"--install-folder={conan_install}",
     "--build=outdated",
     source_dir
-])
+)
 
 print()
 print(f"=== Prepare build (meson/ninja via conan) ===")
-check_call([
-    conan_bin, "build",
+run_conan(
+    "build",
     f"--build-folder={build_dir}",
     f"--install-folder={conan_install}",
     "--configure",  # Only configure, not build (yet)
     source_dir
-])
+)
 
 # Save this to the end, to preserve conan cache for debugging if things fail
 print()
 print(f"=== Clean C++ package cache (conan remove) ===")
-check_call([conan_bin, "remove", "--src", "--builds", "--force", "*"])
+run_conan("remove", "--src", "--builds", "--force", "*")
 
 print()
 print(f"::: Setup complete, build with: ninja -C build :::")
